e483dd6412889f0c97108754ce71c1747318a7d2
agente100p <144070306+marcsVDev@users.noreply.github.com>
2024-08-16 20:55:31 -0300

    Release v1.0 reseted, lib and crypt

--- AGLib/MainCall.cs
+++ AGLib/MainCall.cs
@@ -1,18 +1,42 @@
 ﻿namespace AGCrypt
 {
-    public static class MainCall
+    public class MainCall
     {
-        private static int[] cryptedIndexGB = new int[] { 31, 41, 59, 26, 53, 58, 97, 93, 23, 84, 62, 64, 33, 83, 27, 95, 02, 88, 42, 99, 17, 91, 9, 37, 51, 05, 82, 09, 74, 94, 45, 92, 30, 78, 16, 40, 63, 86, 20, 89, 90, 66, 80, 34, 81, 54, 43, 11, 70 }; // Padrão: baseado no Pi, com algumas alterações
-        private static char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '!', '.', '*', '-', '_', '+', '?', '@', '$', ',', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }; // Variavel Array, armazena o alfabeto QWERT, numeros e caracteres
-        public static string EnCrypt(string text, int key)
+        private int[] cryptedIndexGB = new int[] { 31, 41, 59, 26, 53, 58, 97, 93, 23, 84, 62, 64, 33, 83, 27, 95, 02, 88, 42, 99, 17, 91, 09, 37, 51, 05, 82, 11, 74, 94, 45, 92, 30, 78, 16, 40, 63, 86, 20, 89, 90, 66, 80, 34, 81, 54, 43, 12}; // Padrão: baseado no Pi, com algumas alterações
+        private char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '!', '.', '*', '-', '_', '+', '?', '@', '$', ',', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }; // Variavel Array, armazena o alfabeto QWERT, numeros e caracteres
+        public string EnCrypt(string text, int key)
         {
             Crypt crypt = new Crypt(text, key, cryptedIndexGB, alphabet); // Instacia a classe Crypt, e o objeto recebe os parametros key e text
             return crypt.returnValCrypt();
         }
-        public static string DesCrypt(string disText, int disKey)
+        public string DesCrypt(string disText, int disKey)
         {
             DesCrypt descrypt = new DesCrypt(disText, disKey, cryptedIndexGB, alphabet); // Instacia a classe DesCrypt, e o objeto recebe os parametros key e text
             return descrypt.returnValDesCrypt();
+        }
+        public string ModyACI(char[] alpha, int[] CI)
+        {
+            return alpha.Length != CI.Length ? "The Lenght of the arrays is different!" : verify();
+            string verify()
+            {
+                for (int i = 0; i < alpha.Length; i++)
+                {
+                    for (int j = 0; j < alpha.Length; j++)
+                    {
+                        if (alpha[i].ToString() != alpha[j].ToString() && CI[i] != CI[j] || j == i)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return $"This {alpha[i].ToString()} and {alpha[j].ToString()} or {CI[i]} and {CI[j]} is equal!";
+                        }
+                    }
+                }
+                cryptedIndexGB = CI;
+                alphabet = alpha;
+                return "Seted!";
+            }
         }
     }
 }
--- AGLib/obj/Debug/net7.0/AGLib.AssemblyInfo.cs
+++ AGLib/obj/Debug/net7.0/AGLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AGLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a5081dab25191d4094bb6179e6e6ad01377f99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d43042c62885db5b8910c857d59a69c7a5a949")]
 [assembly: System.Reflection.AssemblyProductAttribute("AGLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AGLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
